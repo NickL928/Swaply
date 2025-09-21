5a675f8dee127170b3088c1de245e6b27762ed6c
Li Xinjun <46631567+NickL928@users.noreply.github.com>
2025-09-21 21:17:30 +0200

    add backend as regular folder

---
+++ Swaply-back-end/src/main/java/com/swaply/swaplybackend/SwaplyBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.swaply.swaplybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaplyBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaplyBackEndApplication.class, args);
+    }
+
+}
---
+++ Swaply-back-end/src/test/java/com/swaply/swaplybackend/SwaplyBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swaply.swaplybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaplyBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
